5a748e68b50bd60aa15e58a2356d060d42441662
armandios19 <fooseaov@gmail.com>
2022-07-07 09:11:21 -0500

    primer clase

--- clases/capitulo_1.R
+++ clases/capitulo_1.R
@@ -1,1 +1,10 @@
-#
+# para realizar el análisis de expresion diferencial existen paqueterias como
+# DESeq2, vamos a cargar la libreria
+
+BiocManager::install("DESeq2")
+
+# abrir paqueterias de trabajo
+library(DESeq2)
+library(tidyverse)
+library(pheatmap)
+library(RColorBrewer)
