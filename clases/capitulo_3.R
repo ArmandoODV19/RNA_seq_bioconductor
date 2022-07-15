036b7e1b50a9d85c61b3d05765ba4c9495b8c5c8
armandios19 <fooseaov@gmail.com>
2022-07-15 16:05:39 -0500

    clase

--- clases/capitulo_3.R
+++ clases/capitulo_3.R
@@ -1,1 +1,13 @@
-#
+# comenzando análisis de expresion diferencial
+# para esto se siguen tres paso
+# 1. adecuar la cuentas crudas para cada gen para el modelo binomial negativo
+# de DESeq2 y probar y examinar expresion diferencial tomando en cuenta
+# los cambios log2 y extraer y visualizar la informacion
+
+# para comenzar el nalisis se necesita el objeto DESeq dds_wt
+# para hacer el ajuste del modelo se utiliza la funcion  DESeq()
+
+dds_wt <- DESeq(dds_wt)
+
+# este modelo contiene la informacion necesaria para realizar los
+# analisis de expresion diferencial entre grupos especificos de muestras
