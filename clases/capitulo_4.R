2c98ab21922f4009f0f26d06703ea8824ec82a68
armandios19 <fooseaov@gmail.com>
2022-07-16 12:40:33 -0500

    clase

--- clases/capitulo_4.R
+++ clases/capitulo_4.R
@@ -1,3 +1,15 @@
 # visualizacion
 
 # heatmap
+# este heatmap solo se realiza con los genes significativos
+
+sig_norm_counts_wt <- normalized_wt_counts[wt_res_sig$ensgene,]
+
+heat_colors <- brewer.pal(6, "YlOrRd")
+
+pheatmap(sig_norm_counts_wt,
+         color = heat_colors,
+         cluster_rows = T,
+         show_rownames = F,
+         annotation = select(wt_metadata, condition),
+         scale = "row")
