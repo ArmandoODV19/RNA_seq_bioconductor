916ec4941d2e556e7a1b86cbbd4357cbec755b32
armandios19 <fooseaov@gmail.com>
2022-07-16 13:56:12 -0500

    clase

--- clases/capitulo_4.R
+++ clases/capitulo_4.R
@@ -23,4 +23,12 @@ pheatmap(sig_norm_counts_wt,
 wt_res_all <- wt_res_all %>%
   mutate(threshold = padj < 0.05)
 
-View(wt_res_all)
+# a continuacion con ggplot2 grafica los valores log2 fold change vs
+# los valores log10 de los p values ajustados
+
+ggplot(wt_res_all, aes(x = log2FoldChange, y = -log10(padj),
+                       color = threshold)) +
+  geom_point()+
+  xlab("log2 fold change") +
+  ylab(" -log10 adjusted p value") +
+  theme(legend.position = "none")
